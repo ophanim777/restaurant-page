42e5cc478cac5ccbd7eada91eed885f839a026b2
ophanim777 <rikyjo49@gmail.com>
2026-01-04 21:39:38 +0700

    fixing

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,29 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-mode: "development",
+  mode: "development",
   entry: "./src/index.js",
+  target: "web",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+
   devServer: {
     static: "./dist",
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        type: "javascript/auto",
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
