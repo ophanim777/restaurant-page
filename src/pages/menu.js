dfd373f87f60e871a685401b85e593f1289022ea
ophanim777 <rikyjo49@gmail.com>
2026-01-11 22:27:57 +0700

    making menu title

---
+++ src/pages/menu.js
@@ -0,0 +1,6 @@
+export default function loadMenu() {
+  const content = document.getElementById('content');
+  const div = document.createElement('div');
+  const title = document.createElement('h1');
+  title.textContent = 'Menu';
+}
