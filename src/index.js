5aa3eb8027c58bb3e4d35ce63def47b3264323f5
ophanim777 <rikyjo49@gmail.com>
2026-01-11 22:40:18 +0700

    delete old content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import loadHome from "./pages/home";
 import loadMenu from "./pages/menu";
 import loadContact from "./pages/contact";
+function clearContent() {
+  document.getElementById('content').textContent = '';
+}
 
 loadHome();
