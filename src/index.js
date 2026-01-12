fdea12ff168b7d57b373f32e550172aeae45876b
ophanim777 <rikyjo49@gmail.com>
2026-01-12 09:45:02 +0700

    fixing id

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import loadContact from "./pages/contact";
 function clearContent() {
   document.getElementById('content').textContent = '';
 }
-document.getElementById('home-btn').addEventListener('click', () => {
+document.getElementById('home').addEventListener('click', () => {
   clearContent();
   loadHome();
   });
 
-document.getElementById('menu-btn').addEventListener('click', () => {
+document.getElementById('menu').addEventListener('click', () => {
   clearContent();
   loadMenu();
 });
 
-document.getElementById('contact-btn').addEventListener('click', () => {
+document.getElementById('contact').addEventListener('click', () => {
   clearContent();
   loadContact();
 });
